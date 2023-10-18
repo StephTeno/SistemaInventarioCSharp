2642f5eae72d5eaafb7e8876b8665be6d2a2405a
StephTeno <StephanieTenorioCarolinaOrozco@gmail.com>
2023-10-18 17:31:47 -0600

    Agregar parte visual

--- Vista/Vistas/Hijos/frmCatalogo.Designer.cs
+++ Vista/Vistas/Hijos/frmCatalogo.Designer.cs
@@ -28,17 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            plTitle = new Panel();
+            plTitles = new Panel();
+            label1 = new Label();
+            panel1 = new Panel();
+            rjTextBox1 = new CustomControls.RJControls.RJTextBox();
+            iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            btnSearch = new CustomControls.RJControls.RJButton();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            dataGridView1 = new DataGridView();
+            plTitle.SuspendLayout();
+            plTitles.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox1).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // plTitle
+            // 
+            plTitle.Controls.Add(plTitles);
+            plTitle.Dock = DockStyle.Top;
+            plTitle.Location = new Point(0, 0);
+            plTitle.Name = "plTitle";
+            plTitle.Size = new Size(1000, 57);
+            plTitle.TabIndex = 1;
+            // 
+            // plTitles
+            // 
+            plTitles.Controls.Add(label1);
+            plTitles.Dock = DockStyle.Left;
+            plTitles.Location = new Point(0, 0);
+            plTitles.Name = "plTitles";
+            plTitles.Size = new Size(166, 57);
+            plTitles.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(9, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Catálogo";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(iconPictureBox1);
+            panel1.Controls.Add(btnSearch);
+            panel1.Controls.Add(rjTextBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 57);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 50);
+            panel1.TabIndex = 2;
+            // 
+            // rjTextBox1
+            // 
+            rjTextBox1.BackColor = Color.CornflowerBlue;
+            rjTextBox1.BorderColor = Color.White;
+            rjTextBox1.BorderFocusColor = Color.HotPink;
+            rjTextBox1.BorderRadius = 15;
+            rjTextBox1.BorderSize = 1;
+            rjTextBox1.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjTextBox1.ForeColor = Color.White;
+            rjTextBox1.Location = new Point(211, 10);
+            rjTextBox1.Margin = new Padding(0);
+            rjTextBox1.Multiline = false;
+            rjTextBox1.Name = "rjTextBox1";
+            rjTextBox1.Padding = new Padding(10, 7, 10, 7);
+            rjTextBox1.PasswordChar = false;
+            rjTextBox1.PlaceholderColor = Color.White;
+            rjTextBox1.PlaceholderText = "Buscar...";
+            rjTextBox1.Size = new Size(562, 31);
+            rjTextBox1.TabIndex = 0;
+            rjTextBox1.Texts = "";
+            rjTextBox1.UnderlinedStyle = false;
+            // 
+            // iconPictureBox1
+            // 
+            iconPictureBox1.BackColor = Color.CornflowerBlue;
+            iconPictureBox1.Enabled = false;
+            iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
+            iconPictureBox1.IconColor = Color.White;
+            iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            iconPictureBox1.IconSize = 25;
+            iconPictureBox1.Location = new Point(790, 14);
+            iconPictureBox1.Name = "iconPictureBox1";
+            iconPictureBox1.Size = new Size(25, 25);
+            iconPictureBox1.TabIndex = 3;
+            iconPictureBox1.TabStop = false;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = Color.CornflowerBlue;
+            btnSearch.BackgroundColor = Color.CornflowerBlue;
+            btnSearch.BorderColor = Color.PaleVioletRed;
+            btnSearch.BorderRadius = 15;
+            btnSearch.BorderSize = 0;
+            btnSearch.FlatAppearance.BorderSize = 0;
+            btnSearch.FlatStyle = FlatStyle.Flat;
+            btnSearch.ForeColor = Color.White;
+            btnSearch.Location = new Point(776, 11);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(50, 30);
+            btnSearch.TabIndex = 4;
+            btnSearch.TextColor = Color.White;
+            btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(dataGridView1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 107);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1000, 517);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(494, 511);
+            dataGridView1.TabIndex = 0;
+            // 
+            // frmCatalogo
+            // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1000, 624);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(panel1);
+            Controls.Add(plTitle);
             Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
             FormBorderStyle = FormBorderStyle.None;
             Name = "frmCatalogo";
             Text = "frmCatalogo";
+            plTitle.ResumeLayout(false);
+            plTitles.ResumeLayout(false);
+            plTitles.PerformLayout();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox1).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel plTitle;
+        private Panel plTitles;
+        private Label label1;
+        private Panel panel1;
+        private CustomControls.RJControls.RJTextBox rjTextBox1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private CustomControls.RJControls.RJButton btnSearch;
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dataGridView1;
     }
 }
--- Vista/Vistas/Hijos/frmHistory.Designer.cs
+++ Vista/Vistas/Hijos/frmHistory.Designer.cs
@@ -28,7 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            plTitle = new Panel();
+            plTitles = new Panel();
+            label1 = new Label();
+            plBotones = new Panel();
+            dgvHistory = new DataGridView();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            btnSig = new FontAwesome.Sharp.IconButton();
+            btnBack = new FontAwesome.Sharp.IconButton();
+            plTitle.SuspendLayout();
+            plTitles.SuspendLayout();
+            plBotones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvHistory).BeginInit();
             SuspendLayout();
+            // 
+            // plTitle
+            // 
+            plTitle.Controls.Add(plTitles);
+            plTitle.Dock = DockStyle.Top;
+            plTitle.Location = new Point(0, 0);
+            plTitle.Name = "plTitle";
+            plTitle.Size = new Size(1000, 57);
+            plTitle.TabIndex = 0;
+            // 
+            // plTitles
+            // 
+            plTitles.Controls.Add(label1);
+            plTitles.Dock = DockStyle.Left;
+            plTitles.Location = new Point(0, 0);
+            plTitles.Name = "plTitles";
+            plTitles.Size = new Size(338, 57);
+            plTitles.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(9, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(325, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Historial de Actividades";
+            // 
+            // plBotones
+            // 
+            plBotones.Controls.Add(btnBack);
+            plBotones.Controls.Add(iconButton1);
+            plBotones.Controls.Add(btnSig);
+            plBotones.Dock = DockStyle.Top;
+            plBotones.Location = new Point(0, 57);
+            plBotones.Name = "plBotones";
+            plBotones.Size = new Size(1000, 35);
+            plBotones.TabIndex = 1;
+            // 
+            // dgvHistory
+            // 
+            dgvHistory.BackgroundColor = Color.White;
+            dgvHistory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvHistory.Dock = DockStyle.Fill;
+            dgvHistory.Location = new Point(0, 92);
+            dgvHistory.Name = "dgvHistory";
+            dgvHistory.RowTemplate.Height = 25;
+            dgvHistory.Size = new Size(1000, 532);
+            dgvHistory.TabIndex = 2;
+            // 
+            // iconButton1
+            // 
+            iconButton1.Dock = DockStyle.Right;
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            iconButton1.FlatAppearance.MouseOverBackColor = Color.LightSteelBlue;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.ForeColor = Color.White;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton1.IconColor = Color.RoyalBlue;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            iconButton1.IconSize = 25;
+            iconButton1.Location = new Point(930, 0);
+            iconButton1.Margin = new Padding(3, 3, 10, 3);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(35, 35);
+            iconButton1.TabIndex = 7;
+            iconButton1.UseVisualStyleBackColor = true;
+            iconButton1.Visible = false;
+            // 
+            // btnSig
+            // 
+            btnSig.Dock = DockStyle.Right;
+            btnSig.FlatAppearance.BorderSize = 0;
+            btnSig.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            btnSig.FlatAppearance.MouseOverBackColor = Color.LightSteelBlue;
+            btnSig.FlatStyle = FlatStyle.Flat;
+            btnSig.ForeColor = Color.White;
+            btnSig.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleRight;
+            btnSig.IconColor = Color.RoyalBlue;
+            btnSig.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            btnSig.IconSize = 25;
+            btnSig.Location = new Point(965, 0);
+            btnSig.Margin = new Padding(10, 3, 3, 3);
+            btnSig.Name = "btnSig";
+            btnSig.Size = new Size(35, 35);
+            btnSig.TabIndex = 6;
+            btnSig.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Dock = DockStyle.Right;
+            btnBack.FlatAppearance.BorderSize = 0;
+            btnBack.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            btnBack.FlatAppearance.MouseOverBackColor = Color.LightSteelBlue;
+            btnBack.FlatStyle = FlatStyle.Flat;
+            btnBack.ForeColor = Color.White;
+            btnBack.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleLeft;
+            btnBack.IconColor = Color.RoyalBlue;
+            btnBack.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            btnBack.IconSize = 25;
+            btnBack.Location = new Point(895, 0);
+            btnBack.Margin = new Padding(3, 3, 10, 3);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(35, 35);
+            btnBack.TabIndex = 8;
+            btnBack.UseVisualStyleBackColor = true;
             // 
             // frmHistory
             // 
@@ -36,13 +156,30 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1000, 624);
+            Controls.Add(dgvHistory);
+            Controls.Add(plBotones);
+            Controls.Add(plTitle);
             Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
             FormBorderStyle = FormBorderStyle.None;
             Name = "frmHistory";
             Text = "frmHistory";
+            plTitle.ResumeLayout(false);
+            plTitles.ResumeLayout(false);
+            plTitles.PerformLayout();
+            plBotones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvHistory).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel plTitle;
+        private Panel plTitles;
+        private Label label1;
+        private Panel plBotones;
+        private DataGridView dgvHistory;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btnSig;
+        private FontAwesome.Sharp.IconButton btnBack;
     }
 }
